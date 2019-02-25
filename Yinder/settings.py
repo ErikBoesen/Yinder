fece636cc7a6bf2804248e6b6a23d78d939bd5c1
ErikBoesen <me@erikboesen.com>
2019-02-24 21:10:35 -0500

    Require login

--- Yinder/settings.py
+++ Yinder/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     context = dict(
         Yinder=User.objects.order_by('?')[0],
