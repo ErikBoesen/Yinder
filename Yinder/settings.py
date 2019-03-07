65ee5a08ba9056e3355de259e8cc32745648c05d
ErikBoesen <me@erikboesen.com>
2019-03-07 11:40:47 -0500

    Try and fix some static file paths

--- Yinder/settings.py
+++ Yinder/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.static',
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- Yinder/urls.py
+++ Yinder/urls.py
@@ -17,11 +17,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import *
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^', include('app.urls')),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^', include('registration.urls')),
     url(r'^admin', admin.site.urls),
-] + static(settings.MEDIA_URL,
-           document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
