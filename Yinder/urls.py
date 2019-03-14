e79b7bac468909c6468d400b660b2d589f406f0b
ErikBoesen <me@erikboesen.com>
2019-03-13 20:34:34 -0400

    Add paths for static files

--- Yinder/urls.py
+++ Yinder/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^', include('registration.urls')),
     url(r'^admin', admin.site.urls),
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
